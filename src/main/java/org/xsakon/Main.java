80bec5cac179328826dd0da748f7c473c9ad3733
Maksym Yarema <macsym2@gmail.com>
2023-08-10 16:10:28 +0300

    feat(Main class): basic menu in CLI

--- src/main/java/org/xsakon/Main.java
+++ src/main/java/org/xsakon/Main.java
@@ -1,7 +1,35 @@
 package org.xsakon;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        boolean keepLooping = true;
+
+        while (keepLooping) {
+            try {
+                displayMenu();
+                String choice = scanner.nextLine();
+                switch (Integer.parseInt(choice)) {
+                    case 1 -> System.out.println("case 1...");
+                    case 2 -> keepLooping = false;
+                    default -> System.out.println(choice + " not a valid option");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage() + ". Please enter a number");
+            }
+        }
+    }
+
+    private static void displayMenu() {
+        System.out.println("""
+                \n
+                Menu:
+                1️⃣ - Test
+                2️⃣ - Exit
+                """);
     }
 }
+
