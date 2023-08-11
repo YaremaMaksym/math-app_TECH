6250071ee38e33ef17309cd73d8a7f8a6fad67eb
Maksym Yarema <macsym2@gmail.com>
2023-08-11 12:01:52 +0300

    feat(ExpressionValidator): new checkIfHasInvalidCharacters method

--- src/main/java/org/xsakon/ExpressionValidator.java
+++ src/main/java/org/xsakon/ExpressionValidator.java
@@ -2,8 +2,42 @@ package org.xsakon;
 
 import java.util.Optional;
 import java.util.Stack;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExpressionValidator {
+    public static void checkCorrectness(String expression) {
+        boolean isCorrect = true;
+
+        Optional<Integer> wrongBracketIndex = checkBracketCorrectness(expression);
+        if (wrongBracketIndex.isPresent()) {
+            highlightError(expression, "Wrong bracket placement", wrongBracketIndex.get());
+            isCorrect = false;
+        }
+
+        Optional<Integer> wrongCharIndex = checkIfHasInvalidCharacters(expression);
+        if (wrongCharIndex.isPresent()) {
+            highlightError(expression, "Expression has invalid characters", wrongCharIndex.get());
+            isCorrect = false;
+        }
+
+        if (isCorrect) {
+            System.out.println("Expression is correct");
+        }
+    }
+
+    private static Optional<Integer> checkIfHasInvalidCharacters(String expression){
+        String regex = "[^0-9x+\\-*/().]";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(expression);
+
+        if (matcher.find()){
+            return Optional.of(matcher.start());
+        }
+        else {
+            return Optional.empty();
+        }
+    }
 
     private static Optional<Integer> checkBracketCorrectness(String expression) {
         Stack<Integer> stack = new Stack<>();
@@ -34,19 +68,5 @@ public class ExpressionValidator {
             System.out.print(" ");
         }
         System.out.println("^ Here");
-    }
-
-    public static void checkCorrectness(String expression) {
-        boolean isCorrect = true;
-
-        Optional<Integer> wrongBracketIndex = checkBracketCorrectness(expression);
-        if (wrongBracketIndex.isPresent()) {
-            highlightError(expression, "Wrong bracket placement", wrongBracketIndex.get());
-            isCorrect = false;
-        }
-
-        if (isCorrect) {
-            System.out.println("Expression is correct");
-        }
     }
 }
