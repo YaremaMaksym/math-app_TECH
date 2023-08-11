caca6257bef5547ac1f7da4ef36a0a26d6874cec
Maksym Yarema <macsym2@gmail.com>
2023-08-11 10:43:24 +0300

    feat(Main class): Enter expression and check bracket correctness methods

--- src/main/java/org/xsakon/Main.java
+++ src/main/java/org/xsakon/Main.java
@@ -13,7 +13,7 @@ public class Main {
                 displayMenu();
                 String choice = scanner.nextLine();
                 switch (Integer.parseInt(choice)) {
-                    case 1 -> System.out.println("case 1...");
+                    case 1 -> enterExpressions();
                     case 2 -> keepLooping = false;
                     default -> System.out.println(choice + " not a valid option");
                 }
@@ -27,9 +27,89 @@ public class Main {
         System.out.println("""
                 \n
                 Menu:
-                1️⃣ - Test
+                1️⃣ - Enter expression
                 2️⃣ - Exit
                 """);
     }
+
+    private static String enterExpressions() {
+        Scanner scanner = new Scanner(System.in);
+        boolean isCorrect = true;
+
+        String expression = scanner.nextLine();
+
+        int wrongBracketIndex = checkBracketCorrectness(expression);
+        if (wrongBracketIndex != -1) {
+            highlightError(expression, "Wrong bracket placement", wrongBracketIndex);
+            isCorrect = false;
+        }
+
+        if (isCorrect) {
+            System.out.println("Expression is correct");
+        }
+
+        return expression;
+    }
+
+    private static int checkBracketCorrectness(String expression) {
+        int index = -1;
+        int numOpenedBrackets = 0;
+        int numClosedBrackets = 0;
+
+        for (Character ch : expression.toCharArray()) {
+            if (ch.toString().equals("(")) {
+                numOpenedBrackets++;
+            }
+            else if (ch.toString().equals(")")) {
+                numClosedBrackets++;
+            }
+        }
+
+        if (numOpenedBrackets < numClosedBrackets) {
+            // returns index of redundant bracket
+            int bracketsLeft = numClosedBrackets - numOpenedBrackets;
+
+            for (int i = 0; i < expression.length(); i++){
+                char ch = expression.charAt(i);
+
+                if (bracketsLeft > 0 && Character.toString(ch).equals(")")){
+                    bracketsLeft--;
+                }
+                else if (bracketsLeft == 0 && Character.toString(ch).equals(")")) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        else if (numOpenedBrackets > numClosedBrackets) {
+            // returns index of redundant bracket
+            int bracketsLeft = numOpenedBrackets - numClosedBrackets;
+
+            for (int i = 0; i < expression.length(); i++){
+                char ch = expression.charAt(i);
+
+                if (bracketsLeft > 0 && Character.toString(ch).equals("(")){
+                    bracketsLeft--;
+                }
+                else if (bracketsLeft == 0 && Character.toString(ch).equals("(")) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+
+        return index;
+    }
+
+    public static void highlightError(String expression, String message, int errorIndex) {
+        System.out.println("Error: " + message);
+        System.out.println(expression);
+        for (int i = 0; i < errorIndex; i++) {
+            System.out.print(" ");
+        }
+        System.out.println("^ Here");
+    }
+
+
 }
 
