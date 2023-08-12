3ad9fe866680a9f6118da378d5402cff9c7039fd
Maksym Yarema <macsym2@gmail.com>
2023-08-12 15:49:47 +0300

    feat(Main.enterExpression): Expressions now saving to DB if they are valid

--- src/main/java/org/xsakon/Main.java
+++ src/main/java/org/xsakon/Main.java
@@ -1,9 +1,17 @@
 package org.xsakon;
+import org.xsakon.expression.Expression;
+import org.xsakon.expression.ExpressionDao;
+import org.xsakon.expression.ExpressionValidator;
+
 import java.util.Scanner;
-import java.util.Stack;
+
+// example expressions
+// 1+(3-4)-25(2*(3-5))
+// 24+(x-2)+4=24*3/(x*-4)
 
 public class Main {
     public static void main(String[] args) {
+        ExpressionDao expressionDao = new ExpressionDao();
         Scanner scanner = new Scanner(System.in);
 
         boolean keepLooping = true;
@@ -14,7 +22,7 @@ public class Main {
                 System.out.print("Choice: ");
                 String choice = scanner.nextLine();
                 switch (Integer.parseInt(choice)) {
-                    case 1 -> enterExpressions();
+                    case 1 -> enterExpressions(expressionDao);
                     case 2 -> keepLooping = false;
                     default -> System.out.println(choice + " not a valid option");
                 }
@@ -33,17 +41,16 @@ public class Main {
                 """);
     }
 
-    private static String enterExpressions() {
+    private static void enterExpressions(ExpressionDao expressionDao) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nEnter your expression: ");
         String expressionStr = scanner.nextLine();
 
-        ExpressionValidator.checkCorrectness(expressionStr);
-
-        ExpressionValidator.checkCorrectness(expression);
-
-        return expression;
+        if (ExpressionValidator.checkCorrectness(expressionStr)){
+            expressionDao.save(new Expression(expressionStr));
+            System.out.println("Expression saved");
+        }
     }
 
 }
---
+++ src/main/java/org/xsakon/expression/ExpressionDao.java
@@ -0,0 +1,24 @@
+package org.xsakon.expression;
+
+import org.xsakon.jdbc.DBConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ExpressionDao {
+    public void save(Expression expression){
+        String query = "INSERT INTO expressions (id, expression) VALUES (nextval('root_sequence'), ?)";
+
+        try (Connection connection = DBConnectionManager.openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, expression.getExpression());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
