781bc0d456bf7c7f1a7ced1bc8ca50558f160bea
Maksym Yarema <macsym2@gmail.com>
2023-08-13 09:58:21 +0300

    feat(Main): new menu for expressions

--- src/main/java/org/xsakon/Main.java
+++ src/main/java/org/xsakon/Main.java
@@ -3,10 +3,11 @@ import org.xsakon.expression.Expression;
 import org.xsakon.expression.ExpressionDao;
 import org.xsakon.expression.ExpressionValidator;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 // example expressions
-// 1+(3-4)-25(2*(3-5))
+// 1+(3-4)=25/(2*(3-5*x))
 // 24+(x-2)+4=24*3/(x*-4)
 
 public class Main {
@@ -18,11 +19,11 @@ public class Main {
 
         while (keepLooping) {
             try {
-                displayMenu();
+                displayMainMenu();
                 System.out.print("Choice: ");
                 String choice = scanner.nextLine();
                 switch (Integer.parseInt(choice)) {
-                    case 1 -> enterExpressions(expressionDao);
+                    case 1 -> enterExpressionMenu(expressionDao);
                     case 2 -> keepLooping = false;
                     default -> System.out.println(choice + " not a valid option");
                 }
@@ -32,25 +33,85 @@ public class Main {
         }
     }
 
-    private static void displayMenu() {
+    private static void displayMainMenu() {
         System.out.println("""
                 \n
-                Menu:
-                1️⃣ - Enter expression
+                Main Menu:
+                1️⃣ - Enter Expression Menu
                 2️⃣ - Exit
                 """);
     }
 
-    private static void enterExpressions(ExpressionDao expressionDao) {
+    private static void displayExpressionMenu() {
+        System.out.println("""
+                \n
+                Expression Menu:
+                1️⃣ - Enter Expression
+                2️⃣ - Enter root for current expression
+                3️⃣ - Exit to Main Menu
+                """);
+    }
+
+    private static void enterExpressionMenu(ExpressionDao expressionDao) {
+        Scanner scanner = new Scanner(System.in);
+        Expression expression = null;
+        boolean keepLooping = true;
+
+        while (keepLooping) {
+            try {
+                displayExpressionMenu();
+
+                if (expression != null){
+                    System.out.println("Current expression: " + expression.getExpression() + "\n");
+                }
+
+                System.out.print("Choice: ");
+                String choice = scanner.nextLine();
+                switch (Integer.parseInt(choice)) {
+                    case 1 -> {
+                        Optional<Expression> expressionOptional = enterExpression(expressionDao);
+                        if (expressionOptional.isPresent()){
+                            expression = expressionOptional.get();
+                        }
+                    }
+                    case 2 -> {
+                        if (expression != null){
+                            enterRoot();
+                        } else {
+                            System.out.println("First enter a valid expression");
+                        }
+                    }
+                    case 3 -> keepLooping = false;
+                    default -> System.out.println(choice + " not a valid option");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage() + ". Please enter a number");
+            }
+        }
+    }
+
+    private static Optional<Expression> enterExpression(ExpressionDao expressionDao) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nEnter your expression: ");
         String expressionStr = scanner.nextLine();
 
         if (ExpressionValidator.checkCorrectness(expressionStr)){
-            expressionDao.save(new Expression(expressionStr));
+            Expression expression = new Expression(expressionStr);
+            expressionDao.save(expression);
             System.out.println("Expression saved");
+
+            return Optional.of(expression);
+        } else {
+            return Optional.empty();
         }
+    }
+
+    private static void enterRoot() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("\nEnter root: ");
+        Double rootValue = scanner.nextDouble();
     }
 
 }
