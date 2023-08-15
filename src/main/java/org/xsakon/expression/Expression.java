5cf3d77a516f09fd847b2d8f302a0c7f7bb040ac
Yarema Maksym <macsym2@gmail.com>
2023-08-15 13:55:09 +0300

    feat: Expression have rootValues parameter

--- src/main/java/org/xsakon/expression/Expression.java
+++ src/main/java/org/xsakon/expression/Expression.java
@@ -1,16 +1,26 @@
 package org.xsakon.expression;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Expression {
     private Long id;
     private final String expression;
+    private List<Double> rootValues;
 
     public Expression(String expression) {
         this.expression = expression;
+        this.rootValues = new ArrayList<>();
     }
 
     public Expression(Long id, String expression) {
         this.id = id;
         this.expression = expression;
+        this.rootValues = new ArrayList<>();
+    }
+
+    public void addRoot(double root) {
+        rootValues.add(root);
     }
 
     public void setId(Long id) {
@@ -23,6 +33,11 @@ public class Expression {
 
     public String getExpression() {
         return expression;
+    }
+
+    public List<Double> getRootValues(){
+        List<Double> list = rootValues;
+        return list;
     }
 
     @Override
