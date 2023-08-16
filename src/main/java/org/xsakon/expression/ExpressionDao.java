1e40c9b3c238c6d9d440b1cf4f1cfca18b70c9c8
Yarema Maksym <macsym2@gmail.com>
2023-08-16 10:28:52 +0300

    feat(ExpressionDao): findByExpression method helps check if expression already present in DB

--- src/main/java/org/xsakon/expression/ExpressionDao.java
+++ src/main/java/org/xsakon/expression/ExpressionDao.java
@@ -3,7 +3,7 @@ package org.xsakon.expression;
 import org.xsakon.jdbc.DBConnectionManager;
 
 import java.sql.*;
-import java.util.Optional;
+import java.util.*;
 
 public class ExpressionDao {
     public Optional<Long> save(Expression expression){
@@ -18,6 +18,32 @@ public class ExpressionDao {
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()){
                 return Optional.of(resultSet.getLong(1));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    public Optional<Expression> findByExpression(String expressionInputStr) {
+        String query = "SELECT * FROM expressions " +
+                "WHERE expression = ?";
+
+        try (Connection connection = DBConnectionManager.openConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setString(1, expressionInputStr);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()){
+                while (resultSet.next()) {
+                    long expressionId = resultSet.getLong("id");
+                    String expressionStr = resultSet.getString("expression");
+
+                    Expression expression = new Expression(expressionId, expressionStr);
+                    return Optional.of(expression);
+                }
             }
 
         } catch (SQLException e) {
